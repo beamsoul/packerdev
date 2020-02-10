e14163306fa9f0b762fdcee3c535a470a0092387
beamsoul <50894237+beamsoul@users.noreply.github.com>
2020-02-10 16:22:27 -0600

    pushing

--- jenkinsfileUpdate.groovy
+++ jenkinsfileUpdate.groovy
@@ -1,6 +1,6 @@
 node{
     stage("Pull Repo") {
-        git "https://github.com/beamsoul/packer.git"
+        git "https://github.com/beamsoul/packerdev.git"
     }
     stage("Build AMI"){
         sh "packer build updated/updated.json"
